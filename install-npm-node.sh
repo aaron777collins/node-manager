f8de7d72877533b8bda30b2b6e6d4a51cd745725
Aaron <aaroncollins@rocketinnovationstudio.ca>
2022-03-16 14:25:42 -0400

    Added checks for npm and nvm and links to install if missing

--- install-npm-node.sh
+++ install-npm-node.sh
@@ -4,19 +4,32 @@ source $HOME/.nvm/nvm.sh;
 ARGUMENT_INFO_TEXT=$'Please enter the npm version and then the node version. \nExample: "install-npm-node.sh 6 12". \n(Note: entering latest for npm and "lts/*" for node is an option)\nExample: install-npm-node.sh latest "lts/*"'
 
 # functions
+# Checks for a command and if it fails, displays the passed in string
+# arg1: command as string
+# arg2: string to output if it fails to find the command
+checkForCommand() {
+    res=$($1 --version | grep 'command not found')
+    if [[ $res -ne '' ]]
+        then
+            echo $2
+            exit 0
+    fi
+}
 
-# code
-if [[ $# -ne 2 ]]
+# Installs the desired nvm version based on the user input
+installVersions() {
+    if [[ $# -ne 2 ]]
     then
         echo "$ARGUMENT_INFO_TEXT"
         exit 0
-fi
+    fi
 
-if [[ $1 -eq 'latest' ]]
-    then
-        npm install -g npm
-    else
-        npm install -g npm@$1
-fi
-nvm install $2
+    npm install -g npm@$1
+    nvm install $2
+}
+
+# code
+checkForCommand 'npm' 'Npm not found. Please download and install Npm at https://docs.npmjs.com/downloading-and-installing-node-js-and-npm'
+checkForCommand 'nvm' 'Nvm not found. Please download and install Nvm at www.nvm.sh'
+installVersions $@
 
